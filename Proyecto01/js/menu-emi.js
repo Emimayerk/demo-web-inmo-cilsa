32613c8fe1701468bda62337780f32fbf52a7979
Emiliano Mayer Viegas <emilianomayerviegas@gmail.com>
2020-10-22 16:23:22 -0300

    Confirmo este cambio

---
+++ Proyecto01/js/menu-emi.js
@@ -0,0 +1,11 @@
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function () {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if (ubicacionPrincipal >= Desplazamiento_Actual){
+        document.getElementById('header-primary').style.top = '0';
+    }
+    else {
+        document.getElementById('header-primary').style.top ='-100px';
+    }
+    ubicacionPrincipal=Desplazamiento_Actual;
+}
