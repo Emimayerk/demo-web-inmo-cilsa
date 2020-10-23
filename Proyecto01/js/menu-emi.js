0b67e3d112606773ef2ccad1a3487b3af93fc570
Emiliano Mayer Viegas <emilianomayerviegas@gmail.com>
2020-10-23 11:38:23 -0300

    confirmo commit

--- Proyecto01/js/menu-emi.js
+++ Proyecto01/js/menu-emi.js
@@ -1,11 +1,13 @@
-let ubicacionPrincipal = window.pageYOffset;
-window.onscroll = function () {
-    let Desplazamiento_Actual = window.pageYOffset;
-    if (ubicacionPrincipal >= Desplazamiento_Actual){
-        document.getElementById('header-primary').style.top = '0';
+document.addEventListener("DOMContentLoaded", ()=>{
+    let ubicacionPrincipal = window.pageYOffset;
+    window.onscroll = function () {
+        let Desplazamiento_Actual = window.pageYOffset;
+        if (ubicacionPrincipal >= Desplazamiento_Actual){
+            document.getElementById('header-primary').style.top = '0px';
+        }
+        else {
+            document.getElementById('header-primary').style.top ='-100px';
+        }
+        ubicacionPrincipal=Desplazamiento_Actual;
     }
-    else {
-        document.getElementById('header-primary').style.top ='-100px';
-    }
-    ubicacionPrincipal=Desplazamiento_Actual;
-}
+})
